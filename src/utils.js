1526cfd0e4494cd12f4e7ff7a7cfafef581001f8
Trevor.Max <Trevor.Max@btg.co.nz>
2019-06-06 17:30:18 +1200

    Issue #master - add loading spin for lookup result

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const debounce = (fn, delay) => {
     let timer = null;
     return (...args) => {
@@ -8,3 +10,19 @@ export const debounce = (fn, delay) => {
         }, delay);
     };
 };
+
+export const LoadingWrapper = ({ loading, children }) =>(
+    <div>
+        { loading ?
+            <div className="margin-fixer">
+                <div className="sk-spinner sk-spinner-wave">
+                    <div className="sk-rect1"/>
+                    <div className="sk-rect2"/>
+                    <div className="sk-rect3"/>
+                    <div className="sk-rect4"/>
+                    <div className="sk-rect5"/>
+                </div>
+            </div> : children
+        }
+    </div>
+)
